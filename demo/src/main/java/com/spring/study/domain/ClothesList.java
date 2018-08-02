27b931ee7c4f6d868c66998ee306f9dbaf88e38f
korea8378 <korea8378@naver.com>
2018-08-02 17:01:34 +0900

    add spring boot jpa h2 CRUD JSON(POJO)

---
+++ demo/src/main/java/com/spring/study/domain/ClothesList.java
@@ -0,0 +1,13 @@
+package com.spring.study.domain;
+
+
+import java.util.List;
+
+public class ClothesList<T> {
+
+    private List<T> list;
+
+    public List<T> getList() {
+        return this.list;
+    }
+}
